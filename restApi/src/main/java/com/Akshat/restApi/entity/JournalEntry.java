d2bad9e9ff48827e6da12d449e866ec4cc393760
Akshat1473 <akshat8958@gmail.com>
2025-09-01 18:39:14 +0530

    Lombod library

--- restApi/src/main/java/com/Akshat/restApi/entity/JournalEntry.java
+++ restApi/src/main/java/com/Akshat/restApi/entity/JournalEntry.java
@@ -4,10 +4,15 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+
 import java.time.LocalDateTime;
 // import java.util.Date;
 
+// Doucument give by mongodb document to create entity
 @Document(collection = "journal_entries")
+@Data
+// also give @Getter,@Setter,@RequirmentArgConstructor,@ToString,@EqualsHashCode,@Value uniquely but @Data contain all in it and lombok scanner scan the method when we create in source file
 // if not write () in document then framework mongo search Jouranl entry in db present in collection otherwise provide collection and the instance is mapped with entities
 public class JournalEntry {
     @Id
@@ -17,29 +22,29 @@ public class JournalEntry {
      private String content;
      private LocalDateTime date;
      
-     public LocalDateTime getDate() {
-        return date;
-    }
-     public void setDate(LocalDateTime date) {
-         this.date = date;
-     }
-     public ObjectId getId() {
-         return id;
-     }
-     public void setId(ObjectId id) {
-         this.id = id;
-     }
-     public String getTitle() {
-         return title;
-     }
-     public void setTitle(String title) {
-         this.title = title;
-     }
-     public String getContent() {
-         return content;
-     }
-     public void setContent(String content) {
-         this.content = content;
-     }
+    //  public LocalDateTime getDate() {
+    //     return date;
+    // }
+    //  public void setDate(LocalDateTime date) {
+    //      this.date = date;
+    //  }
+    //  public ObjectId getId() {
+    //      return id;
+    //  }
+    //  public void setId(ObjectId id) {
+    //      this.id = id;
+    //  }
+    //  public String getTitle() {
+    //      return title;
+    //  }
+    //  public void setTitle(String title) {
+    //      this.title = title;
+    //  }
+    //  public String getContent() {
+    //      return content;
+    //  }
+    //  public void setContent(String content) {
+    //      this.content = content;
+    //  }
      
 }
